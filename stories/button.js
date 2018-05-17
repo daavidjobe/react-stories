e0cb8dd49bc4677c88741349d63155a42b2a74ad
David Jobe <daavid.jobe@gmail.com>
2018-05-17 17:50:55 +0200

    initial commit

---
+++ stories/button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import Button from '../components/button'
+
+storiesOf('Button', module)
+  .add('test', () => <Button />)
+
+
